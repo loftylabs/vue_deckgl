288bda4c2fc1b3b4d3cf94d9b099de2ea5a63ba3
Austin <austin@hirelofty.com>
2020-10-14 15:43:58 -0500

    Remove console.loggins.

--- deckgl/DeckGl.vue
+++ deckgl/DeckGl.vue
@@ -15,7 +15,6 @@ const processChildren = (children) => {
     children.forEach(child => {
         // TODO: To add support for Slotted Layers, we will need to change this to a return type of {} with a map key and maybe a layers key which points to an Array.
         if(child.$options._componentTag === 'Mapbox'){
-            console.log('found it')
             map = child
         }
     });
