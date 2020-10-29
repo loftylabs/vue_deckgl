2ddc2c542c4290597dc14dd9a74eeff7d1d527d0
Austin Hackett <ahackit@gmail.com>
2020-10-29 16:14:45 -0500

    Moves docs to _docs so we can build to docs folder for github pages. Updates nuxt settings to accomodate new base url. Adds default docs folder. Updates gitignore. Updates circleci for docs deploys.

--- _docs/nuxt.config.js
+++ _docs/nuxt.config.js
@@ -2,4 +2,10 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   // [additional nuxt configuration]
+  generate: {
+    dir: '../docs'
+  },
+  router: {
+    base: '/vue_deckgl/'
+  }
 })
