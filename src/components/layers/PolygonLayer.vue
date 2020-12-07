308c887523499815a98ef125aa5cd104aa401ad3
willow <wsmoyer313@gmail.com>
2020-12-07 12:13:19 -0600

    add solid polygon layer component

---
+++ src/components/layers/PolygonLayer.vue
@@ -0,0 +1,15 @@
+<script>
+import { PolygonLayer } from "@deck.gl/layers";
+import BaseLayerMixin from "./BaseLayerMixin";
+
+export default {
+  name: "PolygonLayer",
+  mixins: [BaseLayerMixin],
+  data() {
+    return {
+      typeOfLayer: PolygonLayer,
+    };
+  },
+  render: () => null,
+};
+</script>
