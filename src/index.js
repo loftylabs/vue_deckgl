26d508cf01be3d0eebad01d6d688f639d543726d
willow <wsmoyer313@gmail.com>
2020-12-22 11:10:50 -0600

    export tile layer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import DeckGL from './components/deckgl'
 import Mapbox from './components/mapbox'
-import {BaseLayerMixin, GeoJsonLayer, HexagonLayer, PolygonLayer} from './components/layers'
+import {BaseLayerMixin, GeoJsonLayer, HexagonLayer, PolygonLayer, TileLayer} from './components/layers'
 import { baseViewMixin, MapView} from './components/views'
 
 export default DeckGL
-export {DeckGL, Mapbox, BaseLayerMixin, GeoJsonLayer,  baseViewMixin, MapView, HexagonLayer, PolygonLayer}
+export {DeckGL, Mapbox, BaseLayerMixin, GeoJsonLayer,  baseViewMixin, MapView, HexagonLayer, PolygonLayer, TileLayer}
