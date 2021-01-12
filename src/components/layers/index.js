b13b2e06ff94d23521deb47dff08c1b1b310714e
Austin Hackett <ahackit@gmail.com>
2021-01-12 11:55:50 -0600

    Updates readme. Updates GeoJsonLayer casing.

--- src/components/layers/index.js
+++ src/components/layers/index.js
@@ -1,8 +1,8 @@
 
 import BaseLayerMixin from './BaseLayerMixin'
-import GeoJsonlayer from './GeoJsonlayer'
+import GeoJsonLayer from './GeoJGeoJsonLayersonlayer'
 import PolygonLayer from './PolygonLayer'
 import HexagonLayer from './HexagonLayer'
 import TileLayer from './TileLayer'
 
-export { BaseLayerMixin, GeoJsonlayer, PolygonLayer, HexagonLayer, TileLayer } 
+export { BaseLayerMixin, GeoJsonLayer, PolygonLayer, HexagonLayer, TileLayer } 
